ae565f1b3bb9af1fb7f0eeaadcb50584dc5ccf1f
Zerschta <164579652+Zerschta@users.noreply.github.com>
2026-09-22 21:31:21 +0200

    SettingsMenu

--- Assets/Scripts/MainMenu/ButtonLogic.cs
+++ Assets/Scripts/MainMenu/ButtonLogic.cs
@@ -9,16 +9,30 @@ public class ButtonLogic : MonoBehaviour
 
     [SerializeField] private Button Play;
     [SerializeField] private Button Settings;
+    [SerializeField] private Button BackToMenu;
+    [SerializeField] private GameObject MainMenu;
+    [SerializeField] private GameObject SettingsMenu;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        MainMenu.SetActive(true);
+        SettingsMenu.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         startGame();
+        if (Settings.IsPressed())
+        {
+            MainMenu.SetActive(false);
+            SettingsMenu.SetActive(true);
+        }
+        else if (BackToMenu.IsPressed())
+        {
+            MainMenu.SetActive(true);
+            SettingsMenu.SetActive(false);
+        }
     }
     void startGame()
     {
